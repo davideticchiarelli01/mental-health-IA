6c43a5cb3327e3f1cf75ecfbbfeba919273002a5
davideticchiarelli01 <davideticchiarelli01@gmail.com>
2025-01-25 22:40:13 +0100

    rules induction

---
+++ prolog_scripts/attributes.pl
@@ -0,0 +1,14 @@
+a(age,['Adult','Senior','Youth']).
+a(gender,['Female','Male','Non-binary','Unknown']).
+a(role,['Data Scientist','Designer','HR','Marketing','Project Manager','Sales','Software Engineer']).
+a(industry,['Consulting','Education','Finance','Healthcare','IT','Manufacturing','Retail']).
+a(yoe,['Expert','Junior','Mid','Senior','Unknown']).
+a(work_location,['Hybrid','Onsite','Remote']).
+a(hours_worked_per_week,['Full-Time','Overtime','Part-time','Unknown']).
+a(work_life_balance,['Extreme','High','Low','Minimal','Moderate']).
+a(stress_level,['High','Low','Medium']).
+a(social_isolation,['Extreme','High','Low','Minimal','Moderate']).
+a(satisfaction_with_remote_work,['Neutral','Satisfied','Unsatisfied']).
+a(physical_activity,['Daily','Unknown','Weekly']).
+a(sleep_quality,['Average','Good','Poor']).
+a(region,['Africa','Asia','Europe','North America','Oceania','South America']).
